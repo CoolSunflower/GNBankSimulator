bd055f7696e11b5e0cb7724d2414fcb8deae9c6a
Adarsh Gupta <study.adarshgupta@gmail.com>
2025-02-10 23:50:22 +0530

    Constants

---
+++ Bank/Constants.java
@@ -0,0 +1,8 @@
+package Bank;
+
+public final class Constants {
+    private Constants() {}
+
+    public static final int NUM_BRANCHES = 10;
+    public static final int NUM_UPDATERS_PER_BRANCH = 10;
+}
